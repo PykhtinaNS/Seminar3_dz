af54283dade288a8a27db56e057cd6fd161785f2
Natalya Pykhtina <sovanst4@yandex.ru>
2022-09-01 15:30:18 +0300

    Z19

---
+++ Z19/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет,
+// является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+Console.WriteLine("Введите пятизначное число: ");
+string number = Console.ReadLine();
+if(number.Length != 5) 
+{
+    Console.WriteLine($"Число {number} не является пятизначным");
+    return;
+}
+bool Palindrom = true;
+for(int i = 0, j = number.Length - 1; i < 5 && j > 0; i++, j--) 
+{
+    if(number[i] != number[j]) 
+    {
+        Palindrom = false;
+        break;
+    }
+}
+Console.WriteLine($"{number} -> {(Palindrom ? "да" : "нет")}");
